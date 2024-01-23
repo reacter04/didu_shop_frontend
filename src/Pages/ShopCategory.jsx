5ae4ecff12edf3dd3157f8c23739bb9b59ff2ebc
Radu <raduJSdeveloper@gmail.com>
2024-01-23 18:54:15 +0200

    am finisat si sortarea

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from "react";
 import "./CSS/ShopCategory.css";
 import { ShopContext } from "../Context/ShopContext";
-import dropdown_icon from "../Components/Assets/dropdown_icon.png";
 import Item from "../Components/Item/Item";
 
 const numberOfAddedItems = 4;
@@ -11,33 +10,56 @@ function ShopCategory({ banner, category }) {
   const selectedProducts = allProducts.filter(
     (item) => category === item.category && item
   );
-
-
   const [loadedContent, setLoadedContent] = useState(numberOfAddedItems);
-
-  
   const visibleProducts = selectedProducts.slice(0, loadedContent);
+  const [sortingCriterion, setSortingCriterion] = useState("default");
+  const [filteredProducts, setFilteredProducts] = useState(visibleProducts);
 
   const handleLoadMore = () => {
     loadedContent + numberOfAddedItems <= selectedProducts.length
       ? setLoadedContent((prev) => prev + numberOfAddedItems)
       : setLoadedContent(() => selectedProducts.length);
-  }
+  };
+
+
+  const handleSelectedCriterion = (criterion) => {
+    setSortingCriterion(criterion);
+    let sortedProducts = [...visibleProducts];
+  
+    if (criterion === "default") {
+      setFilteredProducts(sortedProducts);
+    } else if (criterion === "new price") {
+      sortedProducts.sort((a, b) => a.newPrice - b.newPrice);
+      setFilteredProducts(sortedProducts);
+    } else if (criterion === "old price") {
+      sortedProducts.sort((a, b) => a.oldPrice - b.oldPrice);
+      setFilteredProducts(sortedProducts);
+    }
+  };
+  
+
+ console.log(filteredProducts)
+
 
   return (
     <div className="shop-category">
       <img className="shop-category-banner" src={banner} alt="" />
       <div className="shop-category-index-sort">
         <p>
-          <span>{visibleProducts.length} </span>din {selectedProducts.length} de
+          <span>{filteredProducts.length} </span>din {selectedProducts.length} de
           produse
         </p>
         <div className="shop-category-sort">
-          Soreaza dupa <img src={dropdown_icon} alt="dropIcon" />
+          <label htmlFor="sortOptions">Sorteaza dupa:</label>
+          <select onChange={(e)=>handleSelectedCriterion(e.target.value)} name={sortingCriterion}>
+            <option value="default">Sortare implicita</option>
+            <option value="new price">Pret nou</option>
+            <option value="old price">Pret vechi</option>
+          </select>
         </div>
       </div>
       <div className="shop-category-products">
-        {visibleProducts.map((item) => (
+        {filteredProducts.map((item) => (
           <Item
             key={crypto.randomUUID()}
             id={item.id}
@@ -49,7 +71,9 @@ function ShopCategory({ banner, category }) {
         ))}
       </div>
       <div onClick={handleLoadMore} className="shopcategory-load-more">
-        {loadedContent === selectedProducts.length? "Nu mai sunt produse" : "Arata mai mult"}
+        {loadedContent === selectedProducts.length
+          ? "Nu mai sunt produse"
+          : "Arata mai mult"}
       </div>
     </div>
   );
